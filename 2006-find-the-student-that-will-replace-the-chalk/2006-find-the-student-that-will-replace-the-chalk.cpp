6cbecb2b85dc5f23225edb443a759810a9b2be39
Ajay Kumar <94215694+ajay2582@users.noreply.github.com>
2024-09-02 19:25:25 +0530

    Time: 106 ms (17.67%), Space: 76.9 MB (22.4%) - LeetHub

--- 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
+++ 2006-find-the-student-that-will-replace-the-chalk/2006-find-the-student-that-will-replace-the-chalk.cpp
@@ -1,21 +1,29 @@
 class Solution {
 public:
     int chalkReplacer(vector<int>& chalk, int k) {
-        // Calculate the total sum of chalk usage
-        long long totalChalk = accumulate(chalk.begin(), chalk.end(), 0LL);
+        // int ans;
+        int n = chalk.size();
+        long long  sum = accumulate( chalk.begin() , chalk.end() , 0LL );
+         k%=sum;
+
+
+
+        for(int i = 0 ;i < n ;i++){
+            int val = chalk[i];
         
-        // Reduce k by modulo with totalChalk
-        k %= totalChalk;
-        
-        // Find the student who will replace the chalk
-        for (int i = 0; i < chalk.size(); ++i) {
-            if (k < chalk[i]) {
-                return i;
+            if(k > val || k == val){
+                k  -= val;
             }
-            k -= chalk[i];
+            else{
+                // ans = i;
+                // break;
+                return i ;
+            }
+           
+
         }
-        
-        // In case no student is found, which theoretically should never happen
         return 0;
+
+        
     }
 };
