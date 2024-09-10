9b18bc3536afe5cb2a7038812dda23c3ce1e6b9c
Ajay Kumar <94215694+ajay2582@users.noreply.github.com>
2024-09-10 23:50:54 +0530

    Time: 39 ms (60.32%), Space: 35.5 MB (49.64%) - LeetHub

--- 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.cpp
+++ 2903-insert-greatest-common-divisors-in-linked-list/2903-insert-greatest-common-divisors-in-linked-list.cpp
@@ -12,17 +12,17 @@ class Solution {
 public:
     int gcd(int a, int b) 
 { 
-    // Everything divides 0 
+     
     if (a == 0) 
         return b; 
     if (b == 0) 
         return a; 
   
-    // base case 
+     
     if (a == b) 
         return a; 
   
-    // a is greater 
+    
     if (a > b) 
         return gcd(a - b, b); 
     return gcd(a, b - a); 
@@ -40,12 +40,6 @@ public:
             temp = temp ->next ->next;
 
         }
-        return ans;
-        
-      
-
-
-        
-        
+        return ans;     
     }
 };
