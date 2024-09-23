88eb138234abcf846033326349a6567878435bfe
Ajay Kumar <94215694+ajay2582@users.noreply.github.com>
2024-09-23 23:12:25 +0530

    Time: 3 ms (92.27%), Space: 17.5 MB (11.78%) - LeetHub

--- 0179-largest-number/0179-largest-number.cpp
+++ 0179-largest-number/0179-largest-number.cpp
@@ -1,32 +1,50 @@
-#include <vector>
-#include <string>
-#include <algorithm>
-
 class Solution {
 public:
-    string largestNumber(std::vector<int>& nums) {
-        // Convert all integers to strings
-        std::vector<std::string> strNums;
-        for (int num : nums) {
-            strNums.push_back(std::to_string(num));
+bool calling( string & a , string &b){
+    if( a + b > b + a){
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+    string largestNumber(vector<int>& nums) {
+
+      
+       
+        string str ="";
+        vector<string>res;
+        for(int i = 0 ;i < nums.size();i++){
+            
+            res.push_back(to_string(nums[i]));
         }
-
-        // Custom comparator to sort based on concatenated values
-        std::sort(strNums.begin(), strNums.end(), [](std::string& a, std::string& b) {
-            return a + b > b + a;
+        // sort(res.begin(), res.end() , calling( string &a,  string &b));?
+         std::sort(res.begin(), res.end(), [this](std::string& a, std::string& b) {
+            return this->calling(a, b);
         });
-
-        // If the largest number is "0", return "0" (to handle cases like [0, 0])
-        if (strNums[0] == "0") {
+        if(res[0] == "0"){
             return "0";
         }
 
-        // Concatenate the sorted strings
-        std::string result;
-        for (const std::string& str : strNums) {
-            result += str;
+        for(int i = 0 ;i < res.size();i++){
+            str += res[i];
         }
+        // if(str.find("0") != string::npos){
+        //     int index = str.find("0");
+            
+        //     char ch = str[index-1];
+        //     if(str.find(ch , index + 1) != string::npos){
+        //         int index2 = str.find(ch , index + 1);
+        //         int diff = index2 - index;
 
-        return result;
+        //     str.erase(index , diff);
+        //      for(int i = 1 ;i <= diff;i++){
+        //         str += "0";
+        //      }
+        //     }
+        // }
+
+        return str;
+        
     }
 };
