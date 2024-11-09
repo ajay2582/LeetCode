7fd6c3d7c0cec71f9a1b8050f58309abfc5269a8
Ajay Kumar <94215694+ajay2582@users.noreply.github.com>
2024-11-09 14:22:24 +0530

    Time: 7 ms (17.49%), Space: 13.2 MB (41.86%) - LeetHub

--- 0414-third-maximum-number/0414-third-maximum-number.cpp
+++ 0414-third-maximum-number/0414-third-maximum-number.cpp
@@ -5,7 +5,10 @@ public:
 
         // If there are fewer than 3 unique elements, return the largest
         if (st.size() < 3) {
-            return *st.rbegin(); // The largest element
+            auto it = st.end();
+            it--;
+            return *it;
+             // The largest element
         }
         
         // Move reverse iterator to the third largest element
