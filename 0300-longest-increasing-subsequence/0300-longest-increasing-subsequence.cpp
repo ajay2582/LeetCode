aab98a357c5ffc288c8f6444863f9a9d003798b3
Ajay Kumar <94215694+ajay2582@users.noreply.github.com>
2024-11-09 20:20:30 +0530

    Time: 4 ms (77.71%), Space: 12.9 MB (98.72%) - LeetHub

--- 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
+++ 0300-longest-increasing-subsequence/0300-longest-increasing-subsequence.cpp
@@ -1,71 +1,23 @@
 class Solution {
 public:
-
-       int f( int index , int prev ,   vector<int>& nums  ,  vector<vector<int>> &dp ) {
-
-            int n = nums.size();
-
-
-           if( index  ==  n  ) {
-               return 0 ;
-
-
-           }
-
-           if( dp[index][prev + 1 ] != -1) {
-               return dp[index][prev + 1 ];
-
-           }
-
-           int nottake = 0 + f( index +  1 , prev , nums , dp);
-               int take  =  INT_MIN;
-
-           if( prev == -1 || nums[index ] > nums[prev ]) {
-                   take = 1 + f( index + 1 , index , nums , dp);
-
-
-               
-           }
-           
-           return    dp[index][prev  + 1 ] =  max ( nottake , take );
-
-       }
     int lengthOfLIS(vector<int>& nums) {
-         int n = nums.size();
 
-        vector<vector<int>> dp(  n  + 1  , vector<int> ( n + 1 , 0 ));
+        vector<int>temp;
+        temp.push_back(nums[0]);
+        int len = 1 ;
+        for( int i = 1 ; i < nums.size();i++){
+            if( nums[i] > temp.back()) {
+                temp.push_back(nums[i]);
+                len++;
 
-        for( int index  = n -1 ; index >= 0; index--) {
-
-            for( int prev = index -1 ;  prev >=-1 ; prev--) {
-
-               
-           int nottake = 0 +  dp[index +  1][prev + 1] ;
-
-               int take  = 0;
-
-           if( prev == -1 || nums[index ] > nums[prev]) {
-                   take = 1 +  dp[index + 1][index + 1];
-
-
-
-               
-           }
-           
-                 dp[index][prev + 1] =  max ( nottake , take );
-
+            }
+            else {
+                int index = lower_bound(temp.begin() , temp.end() , nums[i]) - temp.begin();
+                temp[index] = nums[i];
 
             }
         }
-
-
-
-        return   dp[ 0 ][-1 + 1];
-
-        // return f( 0 ,  -1 , nums , dp );
-
-
-
+        return len;
         
     }
 };
