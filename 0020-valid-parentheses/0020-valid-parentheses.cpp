3305272328fcd62d401c102b39f6ac4be1284b10
Ajay Kumar <94215694+ajay2582@users.noreply.github.com>
2025-01-07 23:26:15 +0530

    Time: 0 ms (100%), Space: 8.8 MB (27.3%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,43 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        int n =s.length();
+        if(n == 1){
+            return false;
+        }
+        stack<char>st;
+        for(int i = 0 ; i < n;i++){
+            if(s[i] == '(' || s[i] == '[' || s[i] == '{') {
+                st.push(s[i]);
+            }else {
+                if (st.empty()) {
+                    return false; // No matching opening bracket for this closing bracket
+                }
+                char ch2 = st.top();
+                // Validate matching pairs
+                if ((ch2 == '[' && s[i] != ']') || 
+                    (ch2 == '{' && s[i] != '}') || 
+                    (ch2 == '(' && s[i] != ')')) {
+                    return false;
+                }
+                st.pop(); 
+               
+
+            }
+
+        }
+
+    
+        if(st.empty() == true) {
+            return true;
+
+        }
+
+        return false;
+        
+
+
+
+        
+    }
+};
