ed6d32d9b37be85bc49b6d99a0e70c7c7093f76e
Ajay Kumar <94215694+ajay2582@users.noreply.github.com>
2025-04-22 09:43:03 +0530

    Time: 0 ms (100%), Space: 11.3 MB (27.38%) - LeetHub

---
+++ 0151-reverse-words-in-a-string/0151-reverse-words-in-a-string.cpp
@@ -0,0 +1,146 @@
+class Solution {
+public:
+
+            int  firstf( string s) {
+
+                int len = s.length();
+
+                int ans = 0;
+
+                for(int i = 0 ; i < len;i++){
+
+                    if(s[i] != ' ') {
+
+                        ans =  i ;
+
+                        break;
+
+
+                    }
+                }
+
+                return ans ;
+
+
+
+
+
+            }
+
+             int lastf(string s ) {
+
+                int ans = 0;
+                int len = s.length();
+
+                for(int i = len - 1 ; i >= 0 ; i--) {
+
+                    if( s[i] != ' ') {
+
+                        ans = i ;
+                        break;
+
+                    }
+                }
+
+                return ans;
+
+
+            }
+    
+
+    
+    string reverseWords(string s) {
+
+
+       
+
+            int first =  firstf( s );
+          int last =   lastf( s );
+
+             last++;
+             
+
+
+
+
+        
+        // s+=" ";
+
+
+        string ans = "";
+
+
+
+        stack<string>st;
+        int len = s.length();
+        string str = "";
+        bool flag = true;
+        s+=' ';
+
+        for(int i = first ; i <=   last ; i++){
+
+            if(s[i] != ' ') {
+                str+=s[i];
+
+                flag = false ;
+
+
+
+            }
+
+            else {
+
+                if( flag == false ) {
+
+
+                st.push(str);
+                str = "";
+
+                flag = true;
+                
+
+                }
+            }
+        }
+
+        // string temp = "";
+        // for( int j = last ; j >= 0 ; j--) {
+
+        //     if( s[j] != ' ') {
+
+        //         temp += s[j];
+
+        //     }
+
+        //     else {
+
+        //         reverse(temp.begin(), temp.end());
+        //         st.push( temp);
+        //         break;
+
+        //     }
+        // }
+
+        while(st.size() != 1) {
+
+            ans += st.top();
+            st.pop();
+            ans += " ";
+
+        }
+
+        ans += st.top();
+
+                    
+
+        return ans;
+
+
+
+
+       
+
+
+        
+    }
+};
